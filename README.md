8c6868e90a3171854e9bb5e566078c90ea992610
Seguya Nicholus <seguyanicholus2015@gmail.com>
2020-02-26 16:58:43 +0300

    added models ,tests and views

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Circle-CI
+[![Seguya-Nicholus](https://circleci.com/gh/Seguya-Nicholus/Circle-CI.svg?style=svg)](https://circleci.com/gh/Seguya-Nicholus/Circle-CI)
+[![Coverage Status](https://coveralls.io/repos/github/Seguya-Nicholus/Circle-CI/badge.svg?branch=master)](https://coveralls.io/github/Seguya-Nicholus/Circle-CI?branch=master)
---
+++ Test/index.test.js
@@ -0,0 +1,13 @@
+const mark = require("../index");
+
+// test('Testing the Mark is a Number', ()=>{
+//     expect(typeof(mark.marks)).toBe('number');
+// })
+
+test("Testing the Mark is a Number", () => {
+  expect(mark.isValidate()).toBeTruthy();
+});
+
+test("Testing the Mark is a Number", () => {
+  expect(mark.isValidate()).toBeTruthy();
+});
